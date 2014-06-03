7ea1d5f18451646cdbd7a4506b31a383d42bd86d
Luciano Jr <l_u_cio@hotmail.com>
2014-06-03 16:08:02 -0300

    adicionando os arquivos

--- arquivo.c
+++ arquivo.c
@@ -5,13 +5,13 @@
 #include "biblioteca.h"
 
 int main(){
-schema * meta;
-pagina * buffer;
+	schema * meta;
+	pagina * buffer;
 
-meta=iniciameta("file/meta.dat",meta);
-buffer=iniciabuffer("file/data.dat",meta,buffer);
-int a;
-a=imprimetupla(buffer,meta,0,2);
-printf("\n ");
-return 0;
+	meta=iniciameta("file/meta.dat",meta);
+	buffer=iniciabuffer("file/data.dat",meta,buffer);
+	int a;
+	a=imprimetupla(buffer,meta,0,2);
+	printf("\n ");
+	return 0;
 }
